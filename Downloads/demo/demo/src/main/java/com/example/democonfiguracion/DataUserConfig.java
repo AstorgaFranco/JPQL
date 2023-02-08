c7b550d5e52c20cc9439d9aae1c791ff405339d4
punko6666 <astorgapunko@gmail.com>
2023-02-08 16:59:06 -0300

    1commit

--- Downloads/demo/demo/src/main/java/com/example/democonfiguracion/DataUserConfig.java
+++ Downloads/demo/demo/src/main/java/com/example/democonfiguracion/DataUserConfig.java
@@ -1,5 +1,89 @@
 package com.example.democonfiguracion;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@EnableTransactionManagement
 public class DataUserConfig {
+	 @Value("${spring.datasource.username}")
+    private static  String USER_DB ;
+	 @Value("${spring.datasource.password}")
+    private static  String PASS_DB ;
+	 @Value("${spring.datasource.driver.driver-class-name}")
+    private static  String DRIVER_DB ;
+	 @Value("${spring.datasource.url}")
+    private static  String URL_DB ;
+	 @Value("${spring.jpa.hibernate.ddl-auto}")
+	 private static String  DDL_AUTO;
+
+
+
+   
+
+	
+	@Bean
+	public DataSource dataSource() {
+	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+	    dataSource.setDriverClassName(DRIVER_DB);
+	    dataSource.setUsername(USER_DB);
+	    dataSource.setPassword(PASS_DB);
+	    dataSource.setUrl(URL_DB); 
+	    
+	    return dataSource;
+	}
+	
+	@Bean
+	   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	      LocalContainerEntityManagerFactoryBean em 
+	        = new LocalContainerEntityManagerFactoryBean();
+	      em.setDataSource(dataSource());
+	      em.setPackagesToScan(new String[] { "com.baeldung.persistence.model" });
+
+	      JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	      em.setJpaVendorAdapter(vendorAdapter);
+	      em.setJpaProperties(additionalProperties());
+
+	      return em;
+	   }
+	   
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+	    JpaTransactionManager transactionManager = new JpaTransactionManager();
+	    transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+	    return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+	    return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	
+	Properties additionalProperties() {
+	    Properties properties = new Properties();
+	    properties.setProperty("jpa.hibernate.ddl-auto", DDL_AUTO);
+	    properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+	       
+	    return properties;
+	}
+
 
 }
